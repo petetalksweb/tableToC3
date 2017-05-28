e9de1b059dbccad575129cdebac29d9ee22bb97d
jacobpetersen <jacob@jacob-petersen.com>
2017-05-27 21:59:50 -0500

    Refactoring logic to seperate JS file

---
+++ js/tableToC3.js
@@ -0,0 +1,31 @@
+function generateChart(tableId, chartDivId) {
+    var table = document.getElementById('tableToC3');
+    var c3Columns = [];
+    var categories = [];
+    for(let rowIndex = 0; rowIndex < table.rows.length; rowIndex++) {
+        for(let colIndex = 0; colIndex < table.rows[rowIndex].cells.length; colIndex++) {
+            if(rowIndex > 0 && colIndex == 0) {
+                categories.push(table.rows[rowIndex].cells[colIndex].innerHTML);
+            } else if (colIndex > 0) {
+                if(c3Columns.length < colIndex) {
+                    c3Columns.push([]);
+                }
+                c3Columns[colIndex - 1].push(table.rows[rowIndex].cells[colIndex].innerHTML);
+            }
+        }
+    }
+    var chart = c3.generate({
+        bindTo: '#' + chartDivId,
+        data: {
+            columns: c3Columns,
+            type: 'bar'
+        },
+        axis: {
+            x: {
+                type: 'category',
+                categories: categories
+            }
+        }
+    });
+    return chart;
+}
